d448d97dc97c2a677f227ed4c487b08e5013031e
Zee Spencer <zee@zeespencer.com>
2014-01-11 09:57:32 -0800

    Reboot lesson 1

--- lessons/js/snake.js
+++ lessons/js/snake.js
@@ -1,113 +1,81 @@
-var startGame = function() {
-
-  var game = new Game({
-    width: 10,
-    height: 10,
-    pixelSize: 20,
-    canvasId: "snake-game",
-  });
-
-  var apple = randomLocation(game.width, game.height);
-  var snake = [{ top: 0, left: 2, direction: "right" },
-               { top: 0, left: 1, direction: "right" },
-               { top: 0, left: 0, direction: "right" }];
-
-  game.onInput(function(input) {
-    changeDirection(input, snake)
-  });
-
-  game.onTick(function() {
-    snake = moveSnake(snake, game.width, game.height);
-    if (collisionBetwixt(snake[0], apple)) {
-      snake = growSnake(snake);
-      apple = randomLocation(game.width, game.height);
-    } else if(outOfBounds(snake, game.width, game.height) || ateItself(snake)) {
-      game.end();
-    } else {
-      game.draw([{ pixels: snake, color: "green" }, { pixels: [apple], color: "red" }]);
-    }
-  });
-
-  game.start();
-};
-
-var bodyWithoutHead = function(snake) {
-  return snake.slice(1, snake.length);
-};
-
-var ateItself = function(snake) {
-  return bodyWithoutHead(snake).some(function(segment) {
-    return collisionBetwixt(snake[0], segment);
-  });
-};
-
-var growSnake = function(snake) {
-  var lastSegment = snake[snake.length - 1];
-  return snake.concat({ top: lastSegment.top,
-                        left: lastSegment.left,
-                        direction: "none"});
-};
-
-var randomLocation = function(maxLeft, maxTop) {
-  return {
-    top: randomNumberUpTo(maxTop),
-    left: randomNumberUpTo(maxLeft)
-  };
-};
-
-var randomNumberUpTo = function(number) {
-  return Math.floor(Math.random()*number);
-};
-
-var collisionBetwixt = function(coordinateA, coordinateB) {
-  return coordinateA.top == coordinateB.top && coordinateA.left == coordinateB.left;
-};
-
-var changeDirection = function(keyCode, snake) {
-  var direction = Game.KEY_MAPPING[keyCode];
-  if (direction) {
-    snake[0].direction = direction;
-  }
-};
+var draw = function(snakeToDraw, apple) {
+  var drawableSnake = { color: "green", pixels: snakeToDraw };
+  var drawableApple = { color: "red", pixels: [apple] };
+  var drawableObjects = [drawableSnake, drawableApple];
+  CHUNK.draw(drawableObjects);
+}
 
 var moveSegment = function(segment) {
-  if (segment.direction == "down") {
-    return { top: segment.top + 1, left: segment.left, direction: segment.direction };
-  } else if (segment.direction == "right") {
-    return { top: segment.top, left: segment.left + 1, direction: segment.direction };
-  } else if (segment.direction == "up") {
-    return { top: segment.top - 1, left: segment.left, direction: segment.direction };
-  } else if (segment.direction == "left") {
-    return { top: segment.top, left: segment.left - 1, direction: segment.direction };
-  } else {
-    return segment;
+  switch(segment.direction) {
+    case "down":
+      return { top: segment.top + 1, left: segment.left };
+    case "up":
+      return { top: segment.top - 1, left: segment.left };
+    case "right":
+      return { top: segment.top, left: segment.left + 1 }
+    case "left":
+      return { top: segment.top, left: segment.left - 1 }
+    default:
+      return segment;
   }
-};
+}
 
-var previousSegmentsDirection = function(snake, index) {
-  var previousSegment = snake[index-1];
-  if (previousSegment) {
-    return previousSegment.direction;
-  } else {
-    return snake[index].direction;
-  }
-};
+var segmentFurtherForwardThan = function(index, snake) {
+  return snake[index - 1] || snake[index];
+}
 
 var moveSnake = function(snake) {
-  return snake.map(function(segment) {
-    return moveSegment(segment);
-  }).map(function(movedSegment, i) {
-    return { top: movedSegment.top, left: movedSegment.left, direction: previousSegmentsDirection(snake, i)}
+  return snake.map(function(oldSegment, segmentIndex) {
+    var newSegment = moveSegment(oldSegment);
+    newSegment.direction = segmentFurtherForwardThan(segmentIndex, snake).direction;
+    return newSegment;
   });
-};
+}
 
-var outOfBounds = function(snake, width, height) {
-  return snake.some(function(segment) {
-    return segment.left >= width ||
-           segment.left < 0 ||
-           segment.top >= height ||
-           segment.top < 0;
-  });
-};
+var growSnake = function(snake) {
+  var tipOfTailIndex = snake.length - 1;
+  var tipOfTail = snake[snake.length - 1];
+  snake.push({ top: tipOfTail.top, left: tipOfTail.left });
+  return snake;
+}
 
-startGame();
+
+// The last thing we need to do is check to see if the snake ran into itself.
+var ate = function(snake, otherThing) {
+  var head = snake[0];
+  return CHUNK.detectCollisionBetween([head], otherThing);
+}
+
+var advanceGame = function() {
+  var newSnake = moveSnake(snake);
+
+  if (ate(newSnake, snake)) {
+    CHUNK.endGame();
+    alert("Woops! You ate yourself!");
+  }
+  // Now we just have to check if the newSnake ate it's previous self to see if
+  // there was a collision!
+
+  if (ate(newSnake, [apple])) {
+    newSnake = growSnake(newSnake);
+    apple = CHUNK.randomLocation();
+  }
+
+  if (ate(newSnake, CHUNK.gameBoundaries())) {
+    CHUNK.endGame();
+    alert("Woops! you hit a wall!");
+  }
+
+  snake = newSnake;
+  draw(snake, apple);
+}
+
+var changeDirection = function(direction) {
+  snake[0].direction = direction;
+}
+
+var apple = CHUNK.randomLocation();
+var snake = [{ top: 1, left: 0, direction: "down" }, { top: 0, left: 0, direction: "down" }];
+
+CHUNK.executeNTimesPerSecond(advanceGame, 1);
+CHUNK.onArrowKey(changeDirection);
