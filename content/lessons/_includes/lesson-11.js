4649bd68b42f3267404f28c7a9118a0158d9638f
Zee Spencer <zee@zeespencer.com>
2014-02-28 19:01:54 -0800

    switch from alert to CHUNK.flashMessage

--- content/lessons/_includes/lesson-11.js
+++ content/lessons/_includes/lesson-11.js
@@ -36,7 +36,7 @@ var advanceGame = function() {
   snake = moveSnake(snake);
   if (CHUNK.detectCollisionBetween(snake, CHUNK.gameBoundaries())) {
     CHUNK.endGame();
-    alert("Woops! you hit a wall!");
+    CHUNK.flashMessage("Woops! you hit a wall!");
   }
   draw(snake, apple);
 }
