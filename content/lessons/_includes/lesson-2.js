83766a3c911d2b3aa25699970d3de6dad09391aa
Zee Spencer <zee@zeespencer.com>
2014-03-23 09:55:48 -0700

    Extract drawableCollection in lesson 2

--- content/lessons/_includes/lesson-2.js
+++ content/lessons/_includes/lesson-2.js
@@ -1,3 +1,4 @@
 var snake = [{ top: 0, left: 0}];
 var drawableSnake = { color: "green", pixels: snake };
-CHUNK.draw([drawableSnake]);
+var drawableObjects = [drawableSnake];
+CHUNK.draw(drawableObjects);
